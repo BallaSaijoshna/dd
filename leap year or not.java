c8a0a27813672f22daecd9287172020bde808d1b
BallaSaijoshna <saijoshna229@gmail.com>
2025-06-04 11:34:04 +0530

    Add files via upload

---
+++ leap year or not.java
@@ -0,0 +1,13 @@
+import java.uti.Scanner;
+public class Main{
+public static void main(String[]args){
+Scanner sc=newScanner(System.in);
+int y=sc.nextInt();
+if(y%4==0&&y%100 i=0){
+System.out.println("leap year");
+}else{
+System.out.prntln("not a leap year");
+}
+sc.close();
+}
+}
